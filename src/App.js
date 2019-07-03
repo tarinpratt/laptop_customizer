0df6d9141a7a9a7b582325e8ae2aac4eb81f20db
Tarin Pratt <tarinpratt@gmail.com>
2019-07-03 16:55:35 -0700

    finall App

--- src/App.js
+++ src/App.js
@@ -7,37 +7,39 @@ import Summary from './Summary';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      selected: {
-        Processor: {
-            name: '17th Generation Intel Core HB (7 Core with donut spare)',
-            cost: 700
-          },
-        "Operating System": {
-            name: 'Ubuntu Linux 16.04',
-            cost: 200
-          },
-        "Video Card":{
-            name: 'Toyota Corolla 1.5v',
-            cost: 1150.98
-          },
-        Display: {
-            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-            cost: 1500
-          }
-      }
+
+  state = {
+    selected: {
+      Processor: {
+          name: '17th Generation Intel Core HB (7 Core with donut spare)',
+          cost: 700
+        },
+      "Operating System": {
+          name: 'Ubuntu Linux 16.04',
+          cost: 200
+        },
+      "Video Card":{
+          name: 'Toyota Corolla 1.5v',
+          cost: 1150.98
+        },
+      Display: {
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500
+        }
     }
   }
-  updateFeature(feature, newValue) {
+  updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
       selected
     });
   }
+
+
+
   render() {
+console.log(this.props.features)
 
     return (
       <div className="App">
@@ -47,18 +49,20 @@ class App extends Component {
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
             <Features 
             features={this.props.features}
-         
-            onUpdateFeature={this.updateFeature}
+            selected={this.state.selected}
+            updateFeature={this.updateFeature}
             />
           </section>
           <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
             <Summary 
+            selected={this.state.selected}
             />
             <div className="summary__total">
              <TotalLabel />
               <div className="summary__total__value">
                 <Total 
+             selected={this.state.selected}
               />
               </div>
             </div>

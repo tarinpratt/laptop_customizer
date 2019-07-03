27f68de30dde31d52a1311d166d3dbe608555a14
Tarin Pratt <tarinpratt@gmail.com>
2019-07-02 18:33:20 -0700

    added state back

--- src/App.js
+++ src/App.js
@@ -7,12 +7,38 @@ import Summary from './Summary';
 import './App.css';
 
 class App extends Component {
-
-
-
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: {
+        Processor: {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+        "Operating System": {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+        "Video Card":{
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+        Display: {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          }
+      }
+    }
+  }
+  updateFeature(feature, newValue) {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  }
 
   render() {
-
 
     return (
       <div className="App">
@@ -22,15 +48,19 @@ class App extends Component {
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
             <Features 
             features={this.props.features}
+            selected={this.state.selected}
+            updateFeature={this.updateFeature}
             />
           </section>
           <section className="main__summary">
             <h3>NEW GREENLEAF 2018</h3>
-            <Summary />
+            <Summary 
+            selected={this.state.selected}/>
             <div className="summary__total">
              <TotalLabel />
               <div className="summary__total__value">
-                <Total />
+                <Total 
+                selected={this.state.selected}/>
               </div>
             </div>
           </section>

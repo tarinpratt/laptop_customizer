4f1a07c0b90e0ad0a63201e45e54586614023666
Tarin Pratt <tarinpratt@gmail.com>
2019-07-02 17:53:33 -0700

    totalLabel

---
+++ src/TotalLabel.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class TotalLabel extends Component {
+  render() {
+    return <div className="summary__total__label">Your Price: </div>;
+  }
+}
+
+export default TotalLabel;

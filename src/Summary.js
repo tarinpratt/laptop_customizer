3330599049545d7991ae14ca95d93ff2299058e6
Tarin Pratt <tarinpratt@gmail.com>
2019-07-03 16:56:17 -0700

    final summary

--- src/Summary.js
+++ src/Summary.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
 
 class Summary extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      selected: {
-        Processor: {
-            name: '17th Generation Intel Core HB (7 Core with donut spare)',
-            cost: 700
-          },
-        "Operating System": {
-            name: 'Ubuntu Linux 16.04',
-            cost: 200
-          },
-        "Video Card":{
-            name: 'Toyota Corolla 1.5v',
-            cost: 1150.98
-          },
-        Display: {
-            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-            cost: 1500
-          }
-      }
-    }
-  }
+
 
   render() {
 
-return Object.keys(this.state.selected)
+return Object.keys(this.props.selected)
           .map(key => <div className="summary__option" key={key}>
             <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.state.selected[key].name}</div>
+            <div className="summary__option__value">{this.props.selected[key].name}</div>
             <div className="summary__option__cost">
               { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.state.selected[key].cost) }
+                  .format(this.props.selected[key].cost) }
             </div>
         </div>)
   }

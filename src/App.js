4385e49f864c509acbf00a0b90603ca66da8e97c
Tarin Pratt <tarinpratt@gmail.com>
2019-07-04 15:08:16 -0700

    added headers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
+import FeatureHeader from './FeatureHeader';
 import Features from './Features';
 import Header from './Header';
 import Total from './Total';
 import TotalLabel from './TotalLabel';
+import SummaryHeader from './SummaryHeader';
 import Summary from './Summary';
 import './App.css';
 
 class App extends Component {
-
-  state = {
-    selected: {
-      Processor: {
-          name: '17th Generation Intel Core HB (7 Core with donut spare)',
-          cost: 700
-        },
-      "Operating System": {
-          name: 'Ubuntu Linux 16.04',
-          cost: 200
-        },
-      "Video Card":{
-          name: 'Toyota Corolla 1.5v',
-          cost: 1150.98
-        },
-      Display: {
-          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-          cost: 1500
-        }
-    }
+  
+state = {
+      selected: {
+        Processor: {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+        "Operating System": {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+        "Video Card":{
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+        Display: {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          }
+      }
   }
+
   updateFeature = (feature, newValue) => {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
@@ -36,33 +39,30 @@ class App extends Component {
     });
   }
 
-
-
   render() {
-console.log(this.props.features)
 
     return (
       <div className="App">
         <Header />   
         <main>
           <section className="main__form">
-            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            <Features 
+            <FeatureHeader />
+            <Features
             features={this.props.features}
             selected={this.state.selected}
-            updateFeature={this.updateFeature}
+            onUpdateFeature={this.updateFeature}
             />
           </section>
           <section className="main__summary">
-            <h3>NEW GREENLEAF 2018</h3>
+            <SummaryHeader />
             <Summary 
             selected={this.state.selected}
             />
             <div className="summary__total">
              <TotalLabel />
               <div className="summary__total__value">
-                <Total 
-             selected={this.state.selected}
+                <Total   
+                selected={this.state.selected}
               />
               </div>
             </div>

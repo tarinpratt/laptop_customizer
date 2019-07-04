26bd2753f6f69e935a2125059eac769c7e2481f4
Tarin Pratt <tarinpratt@gmail.com>
2019-07-04 15:09:22 -0700

    added Summary Header

---
+++ src/SummaryHeader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class SummaryHeader extends Component {
+  render() {
+    return <h3>NEW GREENLEAF 2018</h3>;
+  }
+}
+
+export default SummaryHeader;

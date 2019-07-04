b5aaf39bf64277bf4079a688709a0d3f428452e3
Tarin Pratt <tarinpratt@gmail.com>
2019-07-04 15:08:56 -0700

    added feature header

---
+++ src/FeatureHeader.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class FeatureHeader extends Component {
+  render() {
+    return <h3>TECH SPECS AND CUSTOMIZATIONS</h3>;
+  }
+}
+
+export default FeatureHeader;

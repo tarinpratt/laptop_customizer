a823b6bbec20f38ddec4449ca55f1c5c10ba2632
Tarin Pratt <tarinpratt@gmail.com>
2019-07-08 18:13:57 -0700

    decomposed

--- src/Total.js
+++ src/Total.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 
 class Total extends Component{
 
-    render(){
-        return Object.keys(this.props.selected)
-        .reduce((acc, curr) => acc + this.props.selected[curr].cost, 0);    
+  render(){
+        const total = Object.keys(this.props.selected)
+        .reduce((acc, curr) => acc + this.props.selected[curr].cost, 0)
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+        .format(total)
+        
+        ; 
+       
 
     }
 }

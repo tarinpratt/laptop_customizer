03d40b50c9c940f078f132578262814d3ab2900f
Tarin Pratt <tarinpratt@gmail.com>
2019-07-08 18:13:05 -0700

    decomposed

--- src/Summary.js
+++ src/Summary.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
+import SummaryItems from './SummaryItems';
 
 class Summary extends Component {
 
-
-  render() {
+render() {
 
 return Object.keys(this.props.selected)
-          .map(key => <div className="summary__option" key={key}>
-            <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.props.selected[key].name}</div>
-            <div className="summary__option__cost">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.props.selected[key].cost) }
-            </div>
-        </div>)
+          .map(key =>
+            <SummaryItems 
+            myKey={key}
+            name={this.props.selected[key].name}
+            cost={this.props.selected[key].cost}
+            /> )
   }
 }
 
